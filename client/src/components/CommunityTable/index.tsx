582efbb73d3b42bfe01d129396d5583519118f01
JamesDeChavez <jamesdechavez23@gmail.com>
2023-03-25 16:33:13 -0700

    finished styling for landing page

--- client/src/components/CommunityTable/index.tsx
+++ client/src/components/CommunityTable/index.tsx
@@ -1,7 +1,5 @@
 import { useQuery } from '@apollo/client'
-import { useEffect } from 'react'
 import { GET_LEADERBOARD } from '../../graphql/query'
-import cache from '../../utils/cache'
 import LifterItem from '../LifterItem'
 import './styles.css'
 
@@ -11,8 +9,8 @@ const CommunityTable = () => {
     const className = 'CommunityTable'
     return (
         <div className={className}>
-            <h2 className={`${className}_header`}>BSD Leaderboard</h2>
-            <div className={`${className}_table`}>
+            <div className={`${className}_container`}>
+                <h2 className={`${className}_header`}>BSD Leaderboard</h2>
                 <div className={`${className}_tableHeaders`}>
                     <p>#</p>
                     <p>User</p>
@@ -21,9 +19,11 @@ const CommunityTable = () => {
                     <p>S</p>
                     <p>D</p>
                 </div>
-                {data && data.leaderboard.leaderboard.map((leader: {userId: number, wilksScore: number}, i: number) => {
-                    return <LifterItem key={i} leader={leader} index={i} />
-                })}
+                <div className={`${className}_table`}>
+                    {data && data.leaderboard.leaderboard.map((leader: {userId: number, wilksScore: number}, i: number) => {
+                        return <LifterItem key={i} leader={leader} index={i} />
+                    })}
+                </div>
             </div>
         </div>
     )
